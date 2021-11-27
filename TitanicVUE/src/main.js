74de9110c6f6942135a300f28f7cbcb4bd8ee2c5
wenqi huang <wenqi100@gmail.com>
2021-11-27 15:48:50 +0100

    Meet the problem of install package, so just remove the previouse version and create a new project to try it again.

--- TitanicVUE/src/main.js
+++ TitanicVUE/src/main.js
@@ -3,12 +3,18 @@
  * Node.js
  * VUE CLI
  * Echarts
+ * BootstrapVue
  */
 
 import Vue from 'vue';
 import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-Vue.config.productionTip = true;
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
     render: h => h(App)
