85a2e799b03203bcd173e848f1ff4f05f50d97d7
george <geor2022@edu.easj.dk>
2021-12-03 11:38:29 +0100

    post command

--- TitanicHTML/room.js
+++ TitanicHTML/room.js
@@ -1,13 +1,13 @@
-function getRoomTemp(){
-    axios({
-      method: 'get',
-      url: 'https://titanicweatherapi.azurewebsites.net/api/Titanic/Recent'
-    })
-    .then(res=>ShowRoomTemp(res))
-    .catch(err=>console.log(err));
+function getRoomTemp() {
+  axios({
+    method: 'get',
+    url: 'https://titanicweatherapi.azurewebsites.net/api/Titanic/Recent'
+  })
+    .then(res => ShowRoomTemp(res))
+    .catch(err => console.log(err));
 }
-function ShowRoomTemp(res){
-  
+function ShowRoomTemp(res) {
+
   document.getElementById('temp').innerHTML = `
     ${JSON.stringify(res.data.temperature)}
     `;
@@ -15,5 +15,24 @@ function ShowRoomTemp(res){
     ${JSON.stringify(res.data.humidity)}
     `;
 }
+function ButtonClick() {
+  axios({
+    method: 'post',
+    url: 'https://titanicweatherapi.azurewebsites.net/api/Titanic/SetCommand',
+    data: {integer:3},
+    headers: {contentType: "application/json; charset=utf-8"}
+    
+    
+    
+  })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+document.getElementById("getdata").addEventListener("click", ButtonClick());
 
 getRoomTemp();
